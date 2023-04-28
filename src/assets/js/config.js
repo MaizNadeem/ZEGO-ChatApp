68e374929395412133a7a00c6dda388a7bf0c59e
MaizNadeem <maiznadeem616@gmail.com>
2023-04-28 22:29:52 +0500

    Implementation Complete.

---
+++ src/assets/js/config.js
@@ -0,0 +1,6 @@
+export const avatarPrefix = "https://storage.zego.im/zim/example/web/assets/"
+
+export const appConfig = {
+  appID: 1202710776, // AppID - type: number
+  appSign: "4a8e6e4392f0b297ff7bb50767b8dda86a56f5a58fdf92e0edfed1d66f72924e", // AppSign - type: 64 byte length string
+}
